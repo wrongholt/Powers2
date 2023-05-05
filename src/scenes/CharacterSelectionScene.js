356e3747992b4457195822485b97f15330226b27
wrongholt <wrongholt@gmail.com>
2023-05-04 22:53:59 -0500

    1 file modified

--- src/scenes/CharacterSelectionScene.js
+++ src/scenes/CharacterSelectionScene.js
@@ -14,32 +14,113 @@ class CharacterSelectionScene extends Phaser.Scene {
     background.setScale(newWidth / 1080);
     background.play(true);
     var newChar = chacterContainer(this);
-    // this.anims.create({
-    //   key: 'characteranimation0',
-    //   frames: this[newChar[0]],
-    //   frameRate: 8,
-    //   repeat: -1,
-    // });
-    // this.anims.create({
-    //   key: 'characteranimation1',
-    //   frames: this[newChar[1]],
-    //   frameRate: 8,
-    //   repeat: -1,
-    // });
-    // newChar[0].anims.play('characteranimation0');
-    // newChar[1].anims.play('characteranimation1');
-    // newChar[2].anims.play('characteranimation2');
-    // newChar[3].anims.play('characteranimation3');
-    // newChar[4].anims.play('characteranimation4');
-    // newChar[5].anims.play('characteranimation5');
-    // newChar[6].anims.play('characteranimation6');
-    // newChar[7].anims.play('characteranimation7');
-    // newChar[8].anims.play('characteranimation8');
-    // newChar[9].anims.play('characteranimation9');
-    // newChar[10].anims.play('characteranimation10');
-    // newChar[11].anims.play('characteranimation11');
-    // newChar[12].anims.play('characteranimation12');
-    // newChar[13].anims.play('characteranimation13');
+    const charArray = [
+      'Argus',
+      'Charity',
+      'Edge',
+      'ElectricMean',
+      'Karrigan',
+      'LarsThundersquat',
+      'Lillith',
+      'Randell',
+      'SharpySharp',
+      'Elkeema',
+      'CaptainNemo',
+      'DejahThoris',
+      'JohnCarter',
+    ];
+    this.anims.create({
+      key: 'characteranimation0',
+      frames: this[charArray[0]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation1',
+      frames: this[charArray[1]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation2',
+      frames: this[charArray[2]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation3',
+      frames: this[charArray[3]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation4',
+      frames: this[charArray[4]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation5',
+      frames: this[charArray[5]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation6',
+      frames: this[charArray[6]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation7',
+      frames: this[charArray[7]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation8',
+      frames: this[charArray[8]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation9',
+      frames: this[charArray[9]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation10',
+      frames: this[charArray[10]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation11',
+      frames: this[charArray[11]],
+      frameRate: 8,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: 'characteranimation12',
+      frames: this[charArray[12]],
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    character0.anims.play('characteranimation0');
+    character1.anims.play('characteranimation1');
+    character2.anims.play('characteranimation2');
+    character3.anims.play('characteranimation3');
+    character4.anims.play('characteranimation4');
+    character5.anims.play('characteranimation5');
+    character6.anims.play('characteranimation6');
+    character7.anims.play('characteranimation7');
+    character8.anims.play('characteranimation8');
+    character9.anims.play('characteranimation9');
+    character10.anims.play('characteranimation10');
+    character11.anims.play('characteranimation11');
+    character12.anims.play('characteranimation12');
     Phaser.Actions.GridAlign(newChar, {
       width: 7,
       height: 2,
@@ -61,7 +142,7 @@ function chacterContainer(scene) {
     'Lillith',
     'Randell',
     'SharpySharp',
-    // 'Elkeema',
+    'Elkeema',
     'CaptainNemo',
     'DejahThoris',
     'JohnCarter',
@@ -69,25 +150,26 @@ function chacterContainer(scene) {
   var newChar = [];
   for (var i = 0; i < charArray.length; ) {
     var character = charArray[i];
-    var group;
-    group = scene.add.group();
-    var rect = scene.add.rectangle(0, 0, 200, 450);
-    var color = Phaser.Display.Color.HexStringToColor('#000000').color;
+    // var group;
+    // group[i] = scene.add.group();
+    // var rect = scene.add.rectangle(0, 0, 200, 450);
+    // var color = Phaser.Display.Color.HexStringToColor('#000000').color;
     console.log(character);
-    rect.setStrokeStyle(2, color);
-    var char = new CharacterClass(
+    // rect.setStrokeStyle(2, color);
+    window['character' + i] = new CharacterClass(
       scene,
       0,
       0,
       character,
       character + 'Idle_000.png'
     );
-    group.add(rect);
-    group.add(char);
+
+    // group[i].add(rect);
+    // group[i].add(char);
     // i % 1 ? 20 : i * 100 + 20,
     // i % 2 ? newHeight / 2 + 20 : 20,
-    char.setScale(0.6);
-    newChar.push(group);
+    window['character' + i].setScale(0.6);
+    newChar.push(window['character' + i]);
     i++;
   }
   console.log('NEW CHAR ARRAY!!!!!!!', newChar);
